c94e217482e2d901016f3358ecc5d78ff2ad5e7b
rushorosh <rusik9813@mail.ru>
2023-03-26 00:58:59 +0300

    Initial Commit

---
+++ rgb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rgb
+//
+//  Created by Русиф on 26.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
